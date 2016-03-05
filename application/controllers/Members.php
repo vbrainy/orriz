bff143145fcb9f4e3470c03eae035f4fcb868ac1
vbrainy <vbrainy.solutions@gmail.com>
2016-03-05 21:21:46 +0530

    image tab

--- application/controllers/Members.php
+++ application/controllers/Members.php
@@ -388,6 +388,52 @@ $this->data['getStep2ProfileDetails'] = $this->member_model->getStep1ProfileDeta
 
     }
     
+    // Upload Image
+    
+     public function upload_profile_image()
+    {
+        $this->load->view('members/edit_uploadimage');
+       // p(51);
+    }
+    
+    
+        public function upload_image()
+    {   if (isset($_POST["upload_thumbnail"])) {
+        $upload_path = "public/images/pic/";
+        $thumb_width = "150";
+        $thumb_height = "150";
+        $filename = $_POST['filename'];
+
+        $large_image_location = $upload_path.$_POST['filename'];
+        $thumb_image_location = "public/images/thumb/".$_POST['filename'];
+
+        $x1 = $_POST["x1"];
+        $y1 = $_POST["y1"];
+        $x2 = $_POST["x2"];
+        $y2 = $_POST["y2"];
+        $w = $_POST["w"];
+        $h = $_POST["h"];
+
+        $scale = $thumb_width/$w;
+        
+        
+        $cropped = resizeThumbnailImage($thumb_image_location, $large_image_location,$w,$h,$x1,$y1,$scale);
+        
+        $data    = [
+
+            'image' => $filename
+
+        ];
+        $user_id = $this->session->userdata('user_id');
+        $this->member_model->update_members_profile($user_id, $data);
+      
+        redirect(base_url('members/upload_profile_image'),'refresh');
+        exit();
+    }
+
+
+    }
+    
     function forgot_password()
     {
         // setting validation rules by checking wheather identity is username or email
